31115366a5d146e892c670362399e17927db6203
dabragamov-ra <dmitriy.abragamov@isobar.com>
2019-02-07 11:46:21 -0500

    sass

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
 	appHeader: 'Messages',
 	fetchingMsg: 'Loading more messages',
 	authorImgAlt: 'Photo of the author',
-	messagesApi: 'http://message-list.appspot.com/'
+	messagesApi: 'https://message-list.appspot.com/'
 }
